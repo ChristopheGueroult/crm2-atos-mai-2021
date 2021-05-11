5225d2916c7b80e19d404c0798d6546d7fe6ce8c
christophe <christophegueroult@gmail.com>
2021-05-11 16:14:24 +0200

    exemple Injection dependencie with class Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { PageForgotComponent } from './login/pages/page-forgot/page-forgot.component';
-import { PageResetComponent } from './login/pages/page-reset/page-reset.component';
-import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
-import { PageSignUpComponent } from './login/pages/page-sign-up/page-sign-up.component';
+import {
+  PreloadAllModules,
+  Router,
+  RouterModule,
+  Routes,
+} from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
-  { path: 'sign-in', component: PageSignInComponent },
-  { path: 'sign-up', component: PageSignUpComponent },
-  { path: 'reset', component: PageResetComponent },
-  { path: 'forgot', component: PageForgotComponent },
+
   {
     path: 'orders',
     loadChildren: () =>
@@ -38,4 +36,10 @@ const routes: Routes = [
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+  // accéder à la propriété public config de la class Router
+  // pour afficher ma config dans la console
+  constructor(private router: Router) {
+    // console.log(this.router.config);
+  }
+}
