e646374a336adda180576d582a07fb4bcada72d6
christophe <christophegueroult@gmail.com>
2021-05-17 17:51:16 +0200

    tp version

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as dayjs from 'dayjs';
 import * as moment from 'moment';
+import { Subject } from 'rxjs';
+import { VersionService } from '../../services/version.service';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -9,7 +11,9 @@ import * as moment from 'moment';
 export class FooterComponent implements OnInit {
   public date = moment().format('dd/mm/YYYY');
   public date2 = dayjs().format('dd/mm/YYYY');
-  constructor() {}
-
+  public version$: Subject<number>;
+  constructor(private versionService: VersionService) {
+    this.version$ = this.versionService.version$;
+  }
   ngOnInit(): void {}
 }
---
+++ src/app/core/services/version.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VersionService {
+  public version$ = new BehaviorSubject(1);
+  constructor() {}
+  public incrementVersion(): void {
+    this.version$.next(this.version$.value + 1);
+    console.log(this.version$.value);
+  }
+}
